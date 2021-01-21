0b4c2acf2a85ca2f6270d3037428b84fc72ab092
Derek <derekcandersen@gmail.com>
2021-01-21 11:33:12 -0500

    change att2fst.sh output to fst_lib/

--- src/fstlib/att2fst.sh
+++ src/fstlib/att2fst.sh
@@ -3,5 +3,5 @@ for f in $FILES
 do
 	echo "Processing $f file..."
 	f2=${f%????}
-	fstcompile --isymbols=ins.txt --osymbols=outs.txt $f  $f2.fst
+	fstcompile --isymbols=ins.txt --osymbols=outs.txt $f  /lib_fst/$f2.fst
 done
