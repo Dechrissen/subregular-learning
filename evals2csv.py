2acaed1460527d051e55c33a75a3298e78e756c3
Sam van der Poel <samrvanderpoel@gmail.com>
2021-06-11 10:40:01 -0400

    Improved csv output and added ROC curve script

--- evals2csv.py
+++ evals2csv.py
@@ -7,7 +7,8 @@ with open(all_evals, 'r') as f:
 
 split_lines = [line.strip().split('/')[1:] for line in lines]
 
-results = []
+results = [['class', 'alph', 'k', 'lang_id', 'direction',
+            'netwrk_type', 'drop', 'size', 'test', 'measure', 'score']]
 
 for line in split_lines:
     i=0
@@ -17,18 +18,22 @@ for line in split_lines:
         direc = 'Bidirectional'
     elif model_type[0] == 'U':
         direc = 'Unidirectional'
-    i_index = model_type.index('i')
-    arch = model_type[i_index+1:]
-    do = pieces[1]
-    lang = pieces[2]
-    lang_class = pieces[2].split('.')[0]
-    set_size = pieces[3]
+    #i_index = model_type.index('i')
+    #arch = model_type[i_index+1:]
+    ntwrk_type = pieces[1]
+    drop = pieces[2]
+    lang_class = pieces[3].split('.')[0]
+    alph = pieces[3].split('.')[1]
+    k = pieces[3].split('.')[2]
+    lang_id = pieces[3].split('.')[3]
+    set_size = pieces[4]
 
     i=1
     test_name = line[i].split('_')[0]
     score_type = line[i].split(':')[1]
     score = float(line[i].split(' ')[-1])
-    new_result = [direc, arch, do, lang_class, lang, set_size, test_name, score_type, score]
+    new_result = [lang_class, alph, k, lang_id, direc, ntwrk_type,
+                  drop, set_size, test_name, score_type, score]
     results.append(new_result)
 
 with open('all_evals.csv', 'w', newline='\n') as f:
