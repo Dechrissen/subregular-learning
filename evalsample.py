90b42ab512b44ae3ed17a6348dae2bfbadabafff
Jeffrey Heinz <jeffrey.heinz@stonybrook.edu>
2021-06-15 20:55:10 -0400

    adjusted pynini function name to work with latest version

--- evalsample.py
+++ evalsample.py
@@ -5,7 +5,7 @@ fsa = pynini.Fst.read("src/fstlib/fst_format/SL.4.2.0.fst")
 
 for line in sys.stdin:
     test_str = line.rstrip()   # have to remove the end of line character (so remove trailing whitespace) 
-    compose = pynini.acceptor(test_str) @ fsa
+    compose = pynini.accep(test_str) @ fsa
     if compose.num_states() == 0:
         is_valid = False
     else:
