1c72cb0dd29583652ec47e90ab237f71d26bbcb7
Sam van der Poel <samrvanderpoel@gmail.com>
2021-06-28 09:18:23 -0400

    made minor edit to lang_names.py

--- src/lang_names.py
+++ src/lang_names.py
@@ -5,3 +5,4 @@ import os
 with open('tags.txt', 'w') as f:
     lang_names = sorted([filename[:-4] for filename in os.listdir("src/fstlib/fst_format/")])
     f.write('\n'.join(lang_names))
+    f.write('\n')
