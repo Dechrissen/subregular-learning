fa796497614b673d75c53567630906d6fde3cc1e
kkostyszyn <kalinakostyszyn@gmail.com>
2021-09-29 13:40:55 -0400

    Changed gen_trials - underscore to spaces

--- gen_trials.py
+++ gen_trials.py
@@ -9,7 +9,7 @@ if __name__ == "__main__":
 	for tag in f:
 		for length in ['1', '10', '100']:
 			for model in ['RNN', 'LSTM', 'GRU']:
-				output_file.write(tag[:-1] + "_" + length + "_" + model + "\n")
+				output_file.write(tag[:-1] + " " + length + " " + model + "\n")
 				
 	output_file.close()
 	print("Done!")
