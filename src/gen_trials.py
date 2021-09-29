b52c3fb8eaf7872d2baecdb65f3b7e49826680ca
kkostyszyn <kalinakostyszyn@gmail.com>
2021-09-29 14:21:45 -0400

    gen_trials fix after move

--- src/gen_trials.py
+++ src/gen_trials.py
@@ -2,7 +2,7 @@ if __name__ == "__main__":
 	print("Opening files....")
 	output_file = open("model_list.txt", "w+", encoding="utf8")
 	
-	f = open("tags.txt", "r", encoding="utf8")
+	f = open("../tags.txt", "r", encoding="utf8")
 	f = f.readlines()
 	
 	print("Generating list....")
