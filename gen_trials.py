b29616f54c0a8ef37dd6ea2222a48bcc0c271a13
kkostyszyn <kalinakostyszyn@gmail.com>
2021-09-29 12:25:06 -0400

    Generated list of models, and fixed typos in README

---
+++ gen_trials.py
@@ -0,0 +1,15 @@
+if __name__ == "__main__":
+	print("Opening files....")
+	output_file = open("model_list.txt", "w+", encoding="utf8")
+	
+	f = open("tags.txt", "r", encoding="utf8")
+	f = f.readlines()
+	
+	print("Generating list....")
+	for tag in f:
+		for length in ['1', '10', '100']:
+			for model in ['RNN', 'LSTM', 'GRU']:
+				output_file.write(tag[:-1] + "_" + length + "_" + model + "\n")
+				
+	output_file.close()
+	print("Done!")
