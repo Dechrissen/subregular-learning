1653b15cd455b5a08d2663df0e40ac6f180770c1
Sam van der Poel <samrvanderpoel@gmail.com>
2021-10-02 08:24:29 -0400

    modified lang_names.py to optionally write lang names to tags.txt for which data generation is not complete

--- src/lang_names.py
+++ src/lang_names.py
@@ -1,8 +1,41 @@
-# Writes all languages in fst_format to tags.txt for convenience
+"""
+Performs one of two tasks:
+    - writes language names to tags.txt for which an fst exists in src/fstlib/fst_format
+      (supply argument --allfst)
+    - writes language names to tags.txt for which data generation is not compelete
+      (supply argument --datagen)
+"""
 
 import os
+import argparse
 
-with open('tags.txt', 'w') as f:
-    lang_names = sorted([filename[:-4] for filename in os.listdir("src/fstlib/fst_format/")])
-    f.write('\n'.join(lang_names))
-    f.write('\n')
+parser = argparse.ArgumentParser()
+parser.add_argument('--allfst', dest='allfst', action='store_true')
+parser.add_argument('--datagen', dest='datagen', action='store_true')
+parser.set_defaults(allfst=False, datagen=False)
+args = parser.parse_args()
+allfst = args.allfst
+datagen = args.datagen
+
+if allfst:
+    with open('tags.txt', 'w') as f:
+        lang_names = sorted([filename[:-4] for filename in os.listdir("src/fstlib/fst_format/")])
+        f.write('\n'.join(lang_names))
+        f.write('\n')
+elif datagen:
+    cwd = os.getcwd()
+    with open('tags.txt', 'r+') as f:
+        langs = [l[:-1] for l in f.readlines()]
+        data1   = [s.split('_')[0] for s in os.listdir(os.path.join(cwd, 'data_gen/1k'))]
+        data10  = [s.split('_')[0] for s in os.listdir(os.path.join(cwd, 'data_gen/10k'))]
+        data100 = [s.split('_')[0] for s in os.listdir(os.path.join(cwd, 'data_gen/100k'))]
+        langs_not_done = []
+        for l in langs:
+            if data1.count(l) != 4 or data10.count(l) != 4 or data100.count(l) != 4:
+                langs_not_done.append(l)
+        out = [l + '\n' for l in langs_not_done]
+        f.seek(0)
+        f.writelines(out)
+        f.truncate()
+else:
+    Warning('No argument supplied.')
