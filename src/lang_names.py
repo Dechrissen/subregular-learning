ea82f775b725b265025b43a13f4e3ce9180ae7a6
Sam van der Poel <svanderpoel@login1.cm.cluster>
2021-10-31 13:17:50 -0400

    update lang_names.py and create manage_datagen.py

--- src/lang_names.py
+++ src/lang_names.py
@@ -3,9 +3,9 @@ Performs one of three tasks:
     - writes language names to tags.txt for which an fst exists in src/fstlib/fst_format
       (supply no argument)
     - writes language names to tags.txt for which data generation is not compelete
-      (supply argument --datagen)
+      (supply --datagen)
     - writes language names to tags.txt for which data generation is complete
-      (supply argument --train)
+      (supply --train)
 """
 
 import os
@@ -19,25 +19,36 @@ args = parser.parse_args()
 datagen = args.datagen
 train = args.train
 
-fname = 'traintags.txt' if train else 'tags.txt'
+lang_names = sorted([filename[:-4] for filename in os.listdir("src/fstlib/fst_format/")])
+file_suffixes = ['Dev','Test1','Test2','Test3','Test4','Training']
+sizes = {'1k':10**3, '10k':10**4, '100k':10**5}
 
-with open(fname, 'w') as f:
-    lang_names = sorted([filename[:-4] for filename in os.listdir("src/fstlib/fst_format/")])
-    out = [l + '\n' for l in lang_names]
-    f.writelines(out)
-cwd = os.getcwd()
-with open(fname, 'r+') as f:
-    langs = [l[:-1] for l in f.readlines()]
-    data1   = [s.split('_')[0] for s in os.listdir(os.path.join(cwd, 'data_gen/1k'))]
-    data10  = [s.split('_')[0] for s in os.listdir(os.path.join(cwd, 'data_gen/10k'))]
-    data100 = [s.split('_')[0] for s in os.listdir(os.path.join(cwd, 'data_gen/100k'))]
-    langs_to_write = []
-    for l in langs:
-        if datagen and (data1.count(l) != 6 or data10.count(l) != 6 or data100.count(l) != 6):
-                langs_to_write.append(l)
-        elif train and data1.count(l) == 6 and data10.count(l) == 6 and data100.count(l) == 6:
-                langs_to_write.append(l)
-    if datagen or train:
+if not (datagen or train):
+    with open('tags.txt', 'w') as f:
+        out = [l + '\n' for l in lang_names]
+        f.writelines(out)
+else:
+    cwd = os.getcwd()
+    with open('tags.txt', 'r+') as f:
+        langs_to_write = []
+        for lang in lang_names:
+            inc = False
+            for suffix in file_suffixes:
+                for size in sizes:
+                    fname = 'data_gen/' + size + '/' + lang + '_' + suffix + '.txt'
+                    if not os.path.exists(fname):
+                        inc = True
+                        break
+                    nlines = len(open(fname, 'r').readlines())
+                    if nlines != sizes[size]:
+                        inc = True
+                        break
+                if inc:
+                    break
+            if datagen and inc:
+                langs_to_write += [lang]
+            elif train and not inc:
+                langs_to_write += [lang]
         out = [l + '\n' for l in langs_to_write]
         f.seek(0)
         f.writelines(out)
---
+++ src/manage_datagen.py
@@ -0,0 +1,39 @@
+#  no arg    : report languages for which datagen is not complete
+#  --repall  : for all languages in fst_format, report how much of
+#              datagen is complete
+#  --rmlangs : remove data files of langs that are not complete
+
+import argparse
+import os
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--repall', dest='repall', action='store_true')
+parser.add_argument('--rmlangs', dest='rmlangs', action='store_true')
+parser.set_defaults(repall=False, rmlangs=False)
+args = parser.parse_args()
+repall = args.repall
+rmlangs = args.rmlangs
+
+lang_names = sorted([filename[:-4] for filename in os.listdir("src/fstlib/fst_format/")])
+file_suffixes = ['Dev','Test1','Test2','Test3','Test4','Training']
+sizes = {'1k':10**3, '10k':10**4, '100k':10**5}
+
+print('LANG' + 20*' ' + '\t'.join(file_suffixes))
+
+for lang in lang_names:
+    ind = {suf:'' for suf in file_suffixes}
+    for suffix in file_suffixes:
+        for size in sizes:
+            fname = 'data_gen/' + size + '/' + lang + '_' + suffix + '.txt'
+            if not os.path.exists(fname):
+                ind[suffix] += '0'
+                continue
+            nlines = len(open(fname, 'r').readlines())
+            ind[suffix] += '1' if nlines == sizes[size] else '0'
+    if repall or any([ind[suf] != '111' for suf in file_suffixes]):
+        print(lang + (24-len(lang))*' ' + '\t'.join([ind[suf] for suf in file_suffixes]))
+    if rmlangs and any([ind[suf] != '111' for suf in file_suffixes]):
+        fnames = ['data_gen/' + size + '/' + lang + '_' + suffix + '.txt' \
+                  for size in sizes for suffix in file_suffixes]
+        for f in fnames:
+            os.remove(f)
