032ed276d41feac483aa13a288220660159a5e88
Sam van der Poel <svanderpoel@login1.cm.cluster>
2022-01-14 09:56:06 -0500

    changes to evals2csv to reflect new lang names

--- src/evals2csv.py
+++ src/evals2csv.py
@@ -1,9 +1,7 @@
 import csv
 
-all_evals = './all_evals.txt'
-
-with open(all_evals, 'r') as f:
-    lines = f.readlines()
+all_evals = 'all_evals.txt'
+lines = open(all_evals, 'r').readlines()
 
 num_rows_out = 0
 for line in lines:
@@ -12,12 +10,12 @@ for line in lines:
 
 split_lines = [line.strip().split('/')[1:] for line in lines]
 
-results = [['class', 'alph', 'k', 'lang_id', 'direction',
-            'netwrk_type', 'drop', 'size', 'test', 'fscore', 'accuracy', 'auc']]
+results = [['alph', 'tier', 'class', 'k', 'j', 'i', 'direction',
+            'network_type', 'drop', 'size', 'test', 'fscore', 'accuracy', 'auc']]
 
 for i in range(0,num_rows_out):
     model = split_lines[i][0]
-    print(model)
+    print(model + '\t\t\t' + str(i))
 
     pieces = model.split('_')
     model_type = pieces[0]
@@ -29,27 +27,25 @@ for i in range(0,num_rows_out):
     #arch = model_type[i_index+1:]
     ntwrk_type = pieces[1]
     drop = pieces[2]
-    lang_class = pieces[3].split('.')[0]
-    alph = pieces[3].split('.')[1]
-    k = pieces[3].split('.')[2]
-    lang_id = pieces[3].split('.')[3]
+    alph = pieces[3].split('.')[0]
+    tier = pieces[3].split('.')[1]
+    lang_class = pieces[3].split('.')[2]
+    k = pieces[3].split('.')[3]
+    j = pieces[3].split('.')[4]
+    lang_i = pieces[3].split('.')[5]
     set_size = pieces[4]
     test_name = split_lines[i][1].split('_')[0]
 
     for line in split_lines:
         if line[0] == model and 'F-score' in line[1] and test_name in line[1]:
             fscore = line[1].split()[1]
-            print(model, fscore)
         elif line[0] == model and 'Accuracy' in line[1] and test_name in line[1]:
             accuracy = line[1].split()[1]
-            print(model, accuracy)
         elif line[0] == model and 'AUC' in line[1] and test_name in line[1]:
             auc = line[1].split()[1]
-            print(model, auc)
 
-    
-    new_result = [lang_class, alph, k, lang_id, direc, ntwrk_type,
-                  drop, set_size, test_name, fscore, accuracy, auc]
+    new_result = [alph, tier, lang_class, k, j, lang_i, direc, ntwrk_type,
+                   drop, set_size, test_name, fscore, accuracy, auc]
     results.append(new_result)
 
 with open('all_evals.csv', 'w', newline='\n') as f:
