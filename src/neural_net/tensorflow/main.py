c7512536dc296334b12790b5b5ce20f59563dec7
Sam van der Poel <samrvanderpoel@gmail.com>
2022-03-30 13:08:53 +0200

    include stack-rnn network type

--- src/neural_net/tensorflow/main.py
+++ src/neural_net/tensorflow/main.py
@@ -1,11 +1,13 @@
 import argparse
 import tensorflow as tf
 import tensorflow.keras as keras
+from tensorflow.keras.callbacks import TensorBoard
 import os.path as path
-import datetime
+from datetime import datetime
 import json
 from data import *
 from model import MainModel
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -36,23 +38,41 @@ if __name__ == "__main__":
     y_train = tf.constant(y_train)
     y_val = tf.constant(y_val)
 
-    config = {'vocab_size': len(vocabulary), 'embed_dim': args.embed_dim,
-            'dropout': args.dropout, 'rnn_type': args.rnn_type, 'bidi': args.bidi}
+    config = {
+        'vocab_size': len(vocabulary),
+        'embed_dim': args.embed_dim,
+        'dropout': args.dropout,
+        'rnn_type': args.rnn_type,
+        'bidi': args.bidi
+    }
 
     model = MainModel(**config)
-    model.compile(optimizer=keras.optimizers.Adam(),
-            loss=keras.losses.BinaryCrossentropy(),
-            metrics=[keras.metrics.BinaryAccuracy()])
+    model.compile(
+        optimizer=keras.optimizers.Adam(),
+        loss=keras.losses.BinaryCrossentropy(),
+        metrics=[keras.metrics.BinaryAccuracy()]
+    )
 
     checkpoint_path = model_path + '/checkpoint.ckpt'
-    checkpoint_callback = tf.keras.callbacks.ModelCheckpoint(filepath=checkpoint_path, save_weights_only=True,
-            monitor='val_acc', mode='max')
+    checkpoint_callback = tf.keras.callbacks.ModelCheckpoint(
+        filepath=checkpoint_path,
+        save_weights_only=True,
+        monitor='val_acc',
+        mode='max'
+    )
 
-    log_dir = model_path + "/logs/fit/" + datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
-    tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir=log_dir, histogram_freq=1)
+    time_now = datetime.now().strftime("%Y%m%d-%H%M%S")
+    log_dir = f"{model_path}/logs/fit/{time_now}"
+    tensorboard_callback = TensorBoard(log_dir=log_dir, histogram_freq=1)
 
-    training_record = model.fit(x_train, y_train, batch_size=args.batch_size, \
-                                epochs=args.epochs, validation_data=(x_val, y_val), callbacks=[checkpoint_callback, tensorboard_callback])
+    training_record = model.fit(
+        x_train,
+        y_train,
+        batch_size=args.batch_size,
+        epochs=args.epochs,
+        validation_data=(x_val, y_val),
+        callbacks=[checkpoint_callback, tensorboard_callback]
+    )
 
     #model.save(model_path)
     save_vocab(vocab_file, vocabulary)
--- src/neural_net/tensorflow/model.py
+++ src/neural_net/tensorflow/model.py
@@ -1,26 +1,50 @@
+import numpy as np
+import tensorflow as tf
+from tensorflow import keras
 from tensorflow.keras import models, layers
 
+
 class MainModel(models.Model):
-    def __init__(self, vocab_size : int, embed_dim : int = 100,
-            dropout : float = 0.2, rnn_type : str = "simple", bidi : bool = False):
+    def __init__(
+        self,
+        vocab_size,
+        embed_dim=100,
+        dropout=0.2,
+        rnn_type="simple",
+        bidi=False
+    ):
         super().__init__()
 
+        self.rnn_type = rnn_type
         self.embeddings = layers.Embedding(vocab_size, embed_dim)
 
-        if rnn_type == "lstm":
-            rnn_layer = layers.LSTM(embed_dim, dropout=dropout)
-        elif rnn_type == "gru":
-            rnn_layer = layers.GRU(embed_dim, dropout=dropout)
-        elif rnn_type == "simple":
-            rnn_layer = layers.SimpleRNN(embed_dim, dropout=dropout)
-        else:
-            raise ValueError("Invalid RNN type")
+        if rnn_type == "stackrnn":
+            rnn_cells = [
+                layers.LSTMCell(embed_dim, dropout=dropout)
+                for _ in range(2)
+            ]
+            stacked_lstm = layers.StackedRNNCells(rnn_cells)
 
-        self.rnn_layer = layers.Bidirectional(rnn_layer) if bidi else rnn_layer
+        rnn_layers = {
+            "lstm":layers.LSTM(embed_dim, dropout=dropout),
+            "gru":layers.GRU(embed_dim, dropout=dropout),
+            "simple":layers.SimpleRNN(embed_dim, dropout=dropout),
+            "stackrnn":layers.RNN(stacked_lstm)
+        }
+
+        try:
+            rnn_layer = rnn_layers[rnn_type]
+        except KeyError:
+            raise ValueError("Invalid RNN type")
+        
+        if bidi:
+            self.rnn_layer = layers.Bidirectional(rnn_layer)
+        else:
+            self.rnn_layer = rnn_layer
         self.linear_layer = layers.Dense(2)
         self.softmax_layer = layers.Softmax()
 
-    def call(self, inputs, training : bool = False):
+    def __call__(self, inputs, value=None, training=False):
         out = self.embeddings(inputs)
         out = self.rnn_layer(out, training=training)
         out = self.linear_layer(out)
