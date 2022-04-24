b604fdc3064bce97588f4da728759da7e80e74b8
Sam van der Poel <samrvanderpoel@gmail.com>
2022-04-24 16:46:56 -0400

    update data collection slurm script

--- collect_evals.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-output=all_evals.txt
-
-find models -name '*_eval.txt' | xargs -I {} grep -He '^F-score' {} | sort | tee $output
-
-find models -name '*_eval.txt' | xargs -I {} grep -He '^Accuracy' {} | sort | tee -a $output
-
-find models -name '*_eval.txt' | xargs -I {} grep -He '^AUC' {} | sort | tee -a $output
